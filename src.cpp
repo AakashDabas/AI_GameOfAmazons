58043be6aa63822e2cbb3d9b628f6f9c62413f32
Aakash Dabas <entdabas@gmail.com>
2016-03-29 11:57:33 +0530

    Branches Cutoff new algo(Prev was competely wrong) *under testing *bug in the move selection from end depth evaluation

--- src.cpp
+++ src.cpp
@@ -3,9 +3,11 @@
 #include<cmath>
 #include<ctime>
 #include<map>
+#include<vector>
 
 #define timeBound   0.98
 #define alphaBeta   true
+#define cutOff  true
 #define inRange(x, y) ( (x >= 0) && (x < 10) && (y >= 0) && (y < 10) )
 #define loop2(itr1, itr2, lim1, lim2) for(; itr1 < lim1; itr1++)  for(; itr2 < lim2; itr2++)
 #define displayMat()  for(int itr1=0; itr1<10; itr1++)    {for(int itr2=0; itr2<10; itr2++)    cout<<mat[itr1][itr2]<<" ";     cout<<endl;}
@@ -22,35 +24,38 @@ map< int[10][10], float> uniTable;  //To implement transposition table
 
 class node{
     public:
-    bool key;
-    int frequency;
-    map<int, node> arr;
+        struct keyNode{
+            int frequency = 0;
+            bool key = false;
+        }dfKeyNode;
+        map<float, keyNode> arr;
+        map<vector<vector<int> >, node> treeRecord;
 
-    void markNodes(bool order, int n=200)
-    {
-        if(!order){
-            map<int, node> :: iterator it = arr.begin();
-            for(int i=0; i<n && it != arr.end(); it++){
-                it->second.key = true;
-                i += it->second.frequency;
+        void markNodes(bool order, int n=50)
+        {
+            if(!order){
+                map<float, keyNode> :: iterator it = arr.begin();
+                for(int i=0; i<n && it != arr.end(); it++){
+                    it->second.key = true;
+                    i += it->second.frequency;
+                }
+            }
+            else if(order){
+                map<float, keyNode> :: iterator it = arr.end();
+                it--;
+                for(int i=0; i<n && it != arr.begin(); it--, i++){
+                    it->second.key = true;
+                    i += it->second.frequency;
+                }
             }
         }
-        else if(order){
-            map<int, node> :: iterator it = arr.end();
-            it--;
-            for(int i=0; i<n && it != arr.begin(); it--, i++){
-                //cout<<it->first<<" "<<it->second.frequency<<endl;
-                it->second.key = true;
-                i += it->second.frequency;
-            }
-        }
-    }
-}defaultVal;
+}dfNode;
 
 class state{
     public:
 
-        int mat[10][10];
+        //int mat[10][10];
+        vector<vector<int> > mat;
 
         struct pos{
             int x,y;
@@ -85,7 +90,7 @@ class state{
                     yOriginal = y;
                     dirX = dirY = -1;
                 }
-                bool generate(int mat[10][10]){
+                bool generate(vector<vector<int> > mat){
                     if(inRange(dirX + x, dirY + y) && mat[dirY + y][dirX + x] == 0) {
                         x += dirX;
                         y += dirY;
@@ -111,9 +116,10 @@ class state{
                 }
         };
 
-        void initialize(int mat[10][10]){
+        void initialize(vector<vector<int> > mat){
             int idx[2];//To store the index of both bots to be feeded in
             idx[1] = idx[0] = 0;
+            this->mat.resize(10, vector<int>(10));
             for(int i=0; i<10; i++)
                 for(int j=0; j<10; j++){
                     this->mat[i][j] = mat[i][j];
@@ -193,73 +199,73 @@ class state{
                 }
             //This is to check for directional moves available
             /*for(int i=0; i<2; i++)
-                for(int j=0; j<4; j++)
-                {
-                    int pX, pY;
-                    pX = position[i][j].x;
-                    pY = position[i][j].y;
-                    int dirX, dirY, x, y, cell = 0;
-                    x = pX;
-                    y = pY;
-                    dirX = dirY = -1;
-                    while(1)
-                    {
-                        if(inRange(dirX + x, dirY + y) && mat[dirY + y][dirX + x] == 0) {
-                            x += dirX;
-                            y += dirY;
-                        }
-                        else{
-                            x = pX;
-                            y = pY;
-                            if(dirX == -1 && dirY == -1)  dirX = 0;
-                            else if(dirX == 0 && dirY == -1)  dirX = 1;
-                            else if(dirX == 1 && dirY == -1)  dirY = 0;
-                            else if(dirX == 1 && dirY == 0)  dirY = 1;
-                            else if(dirX == 1 && dirY == 1)  dirX = 0;
-                            else if(dirX == 0 && dirY == 1)  dirX = -1;
-                            else if(dirX == -1 && dirY == 1)  dirY = 0;
-                            else  break;
-                            if(inRange(dirX + x, dirY + y) && mat[dirY + y][dirX + x] == 0) {
-                                x += dirX;
-                                y += dirY;
-                            }
-                        }
-                        int x2, y2;
-                        int dirX2 = -1, dirY2 = -1;
-                        x2 = x;
-                        y2 = y;
-                        while(1)
-                        {
-                            if(inRange(dirX2 + x2, dirY2 + y2) && mat[dirY2 + y2][dirX2 + x2] == 0) {
-                                x2 += dirX2;
-                                y2 += dirY2;
-                            }
-                            else{
-                                x2 = x;
-                                y2 = y;
-                                if(dirX2 == -1 && dirY2 == -1)  dirX2 = 0;
-                                else if(dirX2 == 0 && dirY2 == -1)  dirX2 = 1;
-                                else if(dirX2 == 1 && dirY2 == -1)  dirY2 = 0;
-                                else if(dirX2 == 1 && dirY2 == 0)  dirY2 = 1;
-                                else if(dirX2 == 1 && dirY2 == 1)  dirX2 = 0;
-                                else if(dirX2 == 0 && dirY2 == 1)  dirX2 = -1;
-                                else if(dirX2 == -1 && dirY2 == 1)  dirY2 = 0;
-                                else  break;
-                                if(inRange(dirX2 + x2, dirY2 + y2) && mat[dirY2 + y2][dirX2 + x2] == 0) {
-                                    x2 += dirX2;
-                                    y2 += dirY2;
-                                }
-                            }
-                            if(mat[y2][x2] == 0)
-                            {
-                                cell++;
-                                mat[y2][x2] = -2;
-                            }
-                        }
-                    }
-                    if(i == 0)  finalValue += cell*2;
-                    else    finalValue -= cell*1.5;
-                }*/
+              for(int j=0; j<4; j++)
+              {
+              int pX, pY;
+              pX = position[i][j].x;
+              pY = position[i][j].y;
+              int dirX, dirY, x, y, cell = 0;
+              x = pX;
+              y = pY;
+              dirX = dirY = -1;
+              while(1)
+              {
+              if(inRange(dirX + x, dirY + y) && mat[dirY + y][dirX + x] == 0) {
+              x += dirX;
+              y += dirY;
+              }
+              else{
+              x = pX;
+              y = pY;
+              if(dirX == -1 && dirY == -1)  dirX = 0;
+              else if(dirX == 0 && dirY == -1)  dirX = 1;
+              else if(dirX == 1 && dirY == -1)  dirY = 0;
+              else if(dirX == 1 && dirY == 0)  dirY = 1;
+              else if(dirX == 1 && dirY == 1)  dirX = 0;
+              else if(dirX == 0 && dirY == 1)  dirX = -1;
+              else if(dirX == -1 && dirY == 1)  dirY = 0;
+              else  break;
+              if(inRange(dirX + x, dirY + y) && mat[dirY + y][dirX + x] == 0) {
+              x += dirX;
+              y += dirY;
+              }
+              }
+              int x2, y2;
+              int dirX2 = -1, dirY2 = -1;
+              x2 = x;
+              y2 = y;
+              while(1)
+              {
+              if(inRange(dirX2 + x2, dirY2 + y2) && mat[dirY2 + y2][dirX2 + x2] == 0) {
+              x2 += dirX2;
+              y2 += dirY2;
+              }
+              else{
+              x2 = x;
+              y2 = y;
+              if(dirX2 == -1 && dirY2 == -1)  dirX2 = 0;
+              else if(dirX2 == 0 && dirY2 == -1)  dirX2 = 1;
+              else if(dirX2 == 1 && dirY2 == -1)  dirY2 = 0;
+              else if(dirX2 == 1 && dirY2 == 0)  dirY2 = 1;
+              else if(dirX2 == 1 && dirY2 == 1)  dirX2 = 0;
+              else if(dirX2 == 0 && dirY2 == 1)  dirX2 = -1;
+              else if(dirX2 == -1 && dirY2 == 1)  dirY2 = 0;
+              else  break;
+              if(inRange(dirX2 + x2, dirY2 + y2) && mat[dirY2 + y2][dirX2 + x2] == 0) {
+              x2 += dirX2;
+              y2 += dirY2;
+              }
+              }
+              if(mat[y2][x2] == 0)
+              {
+              cell++;
+              mat[y2][x2] = -2;
+              }
+              }
+              }
+              if(i == 0)  finalValue += cell*2;
+              else    finalValue -= cell*1.5;
+              }*/
             for(int i=0; i<10; i++)
                 for(int j=0; j<10; j++)
                     if(mat[i][j] == -2)
@@ -303,17 +309,19 @@ class state{
                     while(fire.generate(mat)){
 
                         //To check if state is marked or not
-                        float val = evaluate();
-                        if( depth>1 && enlist.arr[val].key == false)    continue;
-                        else if( depth == 1){
-                            map<float, bool>::iterator it = mappy.begin();
-                            bool detected = false;
-                            for(; it != mappy.end() && !detected; it++)
-                                if(it->first == val)
-                                    detected = true;
-                            mappy[val] = true;
-                            if(!detected)    enlist.arr[val] = defaultVal;
-                            else    enlist.arr[val].frequency++;
+                        if(cutOff){
+                            float val = evaluate();
+                            if( depth>1 && enlist.arr[val].key == false)    continue;
+                            else if( depth == 1){
+                                map<float, bool>::iterator it = mappy.begin();
+                                bool detected = false;
+                                for(; it != mappy.end() && !detected; it++)
+                                    if(it->first == val)
+                                        detected = true;
+                                mappy[val] = true;
+                                if(!detected)    enlist.arr[val] = enlist.dfKeyNode;
+                                else    enlist.arr[val].frequency++;
+                            }
                         }
 
                         if(checkTime() > timeBound)
@@ -322,9 +330,17 @@ class state{
                             return bestPoint;
                         }
                         mat[fire.y][fire.x] = -1;
+                        node enlistNxt = enlist;
+                        if(cutOff){
+                            if(depth == 2)
+                                enlist.treeRecord[mat] = dfNode;
+                            if(depth == 1)     enlistNxt = enlist;
+                            else    enlistNxt = enlist.treeRecord[mat];
+                        }
+                        
                         if(isMaximizer)     //Maximizer layer
                         {
-                            float valTmp = decideMove( pCode%2 + 1, depth - 1, (isMaximizer?false:true), enlist.arr[val], alpha, beta);
+                            float valTmp = decideMove( pCode%2 + 1, depth - 1, (isMaximizer?false:true), enlistNxt, alpha, beta);
                             if(valTmp > bestPoint){
                                 bestPoint = valTmp;
                                 if(topMostLevel)//To register the best move available
@@ -344,7 +360,7 @@ class state{
                         }
                         if(isMaximizer == false)    //Minimizer layer
                         {
-                            float valTmp = decideMove( pCode%2 + 1, depth - 1, (isMaximizer?false:true), enlist.arr[val], alpha, beta);
+                            float valTmp = decideMove( pCode%2 + 1, depth - 1, (isMaximizer?false:true), enlistNxt, alpha, beta);
                             if(valTmp < bestPoint)  bestPoint = valTmp;
                             if(bestPoint < beta)   beta = bestPoint;
                             if(beta <= alpha && alphaBeta)
@@ -366,7 +382,8 @@ class state{
                     position[pCodeTmp-1][i].y=pY;
                 }
             }
-            enlist.markNodes(isMaximizer?true:false);
+            if(depth == 1 && cutOff)
+                enlist.markNodes(isMaximizer?true:false);
             return bestPoint;
         }
 };
@@ -374,7 +391,8 @@ class state{
 int main(){
 
     clock_t begin = clock();
-    int mat[10][10];
+    vector<vector<int> > mat(10, vector<int>(10));
+    cout<<"I'mHere:";
     for(int i=0; i<10; i++)
         for(int j=0; j<10; j++)
             cin>> mat[i][j];
@@ -384,12 +402,14 @@ int main(){
     int i = 1;
     int cntFinal = 0;
     node enlist;
-    defaultVal.key = false;
-    defaultVal.frequency = 0;
     for(i = 1; i<20 && checkTime()<timeBound && contTurn; i++)
     {
         stBegin.decideMove(1, i, true, enlist, INT_MIN, INT_MAX, true);
-        //cout<<"i : "<< i<<" cnt: "<<cnt<<endl;
+        /*cout<<"i : "<< i<<" cnt: "<<cnt<<" t: "<<checkTime()<<endl;
+        cout<<"\n Move selected:-\n";
+        cout<<stBegin.finalMove.y1<<" "<<stBegin.finalMove.x1<<endl;
+        cout<<stBegin.finalMove.y2<<" "<<stBegin.finalMove.x2<<endl;
+        cout<<stBegin.finalMove.f2<<" "<<stBegin.finalMove.f1<<endl;*/
         if(contTurn)
         {
             stBegin.finalMove = stBegin.moveTmp;
